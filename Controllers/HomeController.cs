f920d6576cd5d57e86c65e15b965fcdba6f44da7
len-muramatsu <len.muramatsu@gmail.com>
2024-11-25 16:11:56 +0100

    ajout du projet

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using TPLOCAL1.Models;
+
+//Subject is find at the root of the project and the logo in the wwwroot/ressources folders of the solution
+//--------------------------------------------------------------------------------------
+//Careful, the MVC model is a so-called convention model instead of configuration,
+//The controller must imperatively be name with "Controller" at the end !!!
+namespace TPLOCAL1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //methode "naturally" call by router
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                //return to the Index view (see routing in Program.cs)
+                return View();
+            else
+            {
+                //Call different pages, according to the id pass as parameter
+                switch (id)
+                {
+                    case "OpinionList":
+                        //TODO : code reading of the xml files provide
+                        return View(id);
+                    case "Form":
+                        //TODO : call the Form view with data model empty
+                        return View(id);
+                    default:
+                        //return to the Index view (see routing in Program.cs)
+                        return View();
+                }
+            }
+        }
+
+
+        //method to send datas from form to validation page
+        [HttpPost]
+        public ActionResult ValidationFormulaire(FormModel model)
+        {
+            //TODO : test if model's fields are set
+            //if not, display an error message and stay on the form page
+            //else, call ValidationForm with the datas set by the user
+
+            if (model.Address == null || model.Address.Length < 5)
+            {
+                ModelState.AddModelError("", "address too short");
+                return this.View();
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+
+                    
+                
+                return this.View();
+
+            }
+            else
+            {
+                return this.View("ValidationFormulaire");
+            }
+
+        }
+    }
+}
---
+++ Models/FormModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TPLOCAL1.Models
+{
+    public class FormModel
+    {
+        [Required(ErrorMessage = "Entrez un nom.")]
+        public string? Name { get; set; }
+
+        [StringLength(60, ErrorMessage = "Le nombre de caractères maximal est de {0}.")]
+        [Required(ErrorMessage = "Entrez un prénom.")]
+        public string? Forename { get; set; }
+
+        [Required(ErrorMessage = "Choisissez un genre.")]
+        public string? Gender { get; set; }
+
+        [Required(ErrorMessage = "Entrez une adresse.")]
+        public string? Address { get; set; }
+
+        [RegularExpression(@"\d{5}", ErrorMessage = "Le code postal doit être composé de 5 chiffres.")]
+        public int? ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Entrez une ville.")]
+        public string? Town { get; set; }
+
+        [Required(ErrorMessage = "Entrez une adresse email.")]
+        [EmailAddress(ErrorMessage = "Le format de l'adresse email n'est pas correct.")]
+        public string? EmailAdress { get; set; }
+
+        [DataType(DataType.Date)]
+        [RegularExpression(@"(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19[5-9][0-9]|20[0-2][0-1]|2021)")]
+        public DateTime DateDebutFormation { get; set; }
+
+        [Required(ErrorMessage = "Chhoisissez une formation.")]
+        public string? Formation { get; set; }
+
+        [Required(ErrorMessage = "Entrez un avis.")]
+        public string? AvisFormationCobol { get; set; }
+
+        [Required(ErrorMessage = "Entrez un avis.")]
+        public string? AvisFormationCs { get; set; }
+    }
+}
